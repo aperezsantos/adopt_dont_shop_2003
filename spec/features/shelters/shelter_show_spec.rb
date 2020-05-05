e67484338b0297a48eb3d6b62f862ccf15f1a82b
Ana Perez Santos <a.jperez@yahoo.com>
2020-05-04 22:49:54 -0600

    US 3 complete: shelter show page

---
+++ spec/features/shelters/shelter_show_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "Shelter Show Page" do
+  describe "as a visitor when I visit a shelter's show page" do
+    it "I can see that shelter's name and location information" do
+      shelter_1 = Shelter.create(name: "Cat Care Society", address: "5787 W 6th Ave", city: "Lakewood", state: "CO", zip: "80214")
+      shelter_2 = Shelter.create(name: "Foothils Animal Shelter", address: "580 McIntyre St", city: "Golden", state: "CO", zip: "80401")
+
+      visit "/shelters/#{shelter_1.id}"
+
+      expect(page).to have_content(shelter_1.name)
+      expect(page).to have_content(shelter_1.address)
+      expect(page).to have_content(shelter_1.city)
+      expect(page).to have_content(shelter_1.state)
+      expect(page).to have_content(shelter_1.zip)
+      expect(page).to_not have_content(shelter_2.name)
+    end
+  end
+end
+
+
+
+# User Story 3, Shelter Show
+#
+# As a visitor
+# When I visit '/shelters/:id'
+# Then I see the shelter with that id including the shelter's:
+# - name
+# - address
+# - city
+# - state
+# - zip
