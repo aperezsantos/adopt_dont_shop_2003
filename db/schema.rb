107e1a29f584f5df551bc0735c2a4191c47cc1f8
Ana Perez Santos <a.jperez@yahoo.com>
2020-05-07 14:35:46 -0600

    Remove shelter column from pets table (updates schema), update pet model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200507173259) do
+ActiveRecord::Schema.define(version: 20200507185808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20200507173259) do
     t.string "name"
     t.string "age"
     t.string "sex"
-    t.string "shelter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "shelter_id"
