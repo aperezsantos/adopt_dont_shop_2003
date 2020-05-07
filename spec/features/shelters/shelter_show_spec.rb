5ce9375586dbaf1f9578d82cc325bdb57dfcf3c4
Ana Perez Santos <a.jperez@yahoo.com>
2020-05-06 23:35:16 -0600

    Add shelter show page test for updating a shelter

--- spec/features/shelters/shelter_show_spec.rb
+++ spec/features/shelters/shelter_show_spec.rb
@@ -15,9 +15,51 @@ RSpec.describe "Shelter Show Page" do
       expect(page).to have_content(shelter_1.zip)
       expect(page).to_not have_content(shelter_2.name)
     end
+
+    it "I can click on a link that lets me update that shelter using a form" do
+      shelter = Shelter.create(name: "Cat Care Society", address: "5787 W 6th Ave", city: "Lakewood", state: "CO", zip: "80214")
+
+      visit "/shelters/#{shelter.id}"
+
+      click_link "Update Shelter"
+
+      expect(current_path).to eq("/shelters/#{shelter.id}/edit")
+
+      fill_in "name", with: "Updated Shelter Name"
+      fill_in "address", with: "Updated Shelter Address"
+      fill_in "city", with: "Updated Shelter City"
+      fill_in "state", with: "Updated Shelter State"
+      fill_in "zip", with: "Updated Shelter Zip"
+
+      click_on "Submit Update"
+
+      expect(current_path).to eq("/shelters/#{shelter.id}")
+      expect(page).to have_content("Updated Shelter Name")
+      expect(page).to have_content("Updated Shelter Address")
+      expect(page).to have_content("Updated Shelter City")
+      expect(page).to have_content("Updated Shelter State")
+      expect(page).to have_content("Updated Shelter Zip")
+    end
   end
 end
 
+# User Story 5, Shelter Update
+#
+# As a visitor
+# When I visit a shelter show page
+# Then I see a link to update the shelter "Update Shelter"
+# When I click the link "Update Shelter"
+# Then I am taken to '/shelters/:id/edit' where I  see a form to edit the shelter's data including:
+# - name
+# - address
+# - city
+# - state
+# - zip
+# When I fill out the form with updated information
+# And I click the button to submit the form
+# Then a `PATCH` request is sent to '/shelters/:id',
+# the shelter's info is updated,
+# and I am redirected to the Shelter's Show page where I see the shelter's updated info
 
 
 # User Story 3, Shelter Show
