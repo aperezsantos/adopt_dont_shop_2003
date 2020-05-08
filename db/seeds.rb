29def6b99b70fab9af721e7e036737747616c665
Ana Perez Santos <a.jperez@yahoo.com>
2020-05-07 23:55:54 -0600

    Update pets index spec, update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Shelter.destroy_all
+Pet.destroy_all
+
 shelter_1 = Shelter.create(name: "Cat Care Society", address: "5787 W 6th Ave", city: "Lakewood", state: "CO", zip: "80214")
 shelter_2 = Shelter.create(name: "Foothils Animal Shelter", address: "580 McIntyre St", city: "Golden", state: "CO", zip: "80401")
+
+bishi = shelter_1.pets.create(name: "Bishi", age: "10", sex: "M", image: "https://ih.constantcontact.com/fs159/1104169690227/img/112.jpg?a=1118551010932")
+simba = shelter_1.pets.create(name: "Simba", age: "2", sex: "M", image: "https://images.pexels.com/photos/736532/pexels-photo-736532.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260")
+starla = shelter_2.pets.create(name: "Starla", age: "18", sex: "F", image: "https://images.pexels.com/photos/1605481/pexels-photo-1605481.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260")
+jasper = shelter_2.pets.create(name: "Jasper", age: "11", sex: "M", image: "https://images.pexels.com/photos/1543793/pexels-photo-1543793.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260")
--- spec/features/pets/pets_index_spec.rb
+++ spec/features/pets/pets_index_spec.rb
@@ -6,21 +6,22 @@ RSpec.describe "Pets Index Page" do
       shelter_1 = Shelter.create(name: "Cat Care Society", address: "5787 W 6th Ave", city: "Lakewood", state: "CO", zip: "80214")
       shelter_2 = Shelter.create(name: "Foothils Animal Shelter", address: "580 McIntyre St", city: "Golden", state: "CO", zip: "80401")
 
-      bishi = shelter_1.pets.create(name: "Bishi", age: "10", sex: "M", image: "insert image here")
-      simba = shelter_1.pets.create(name: "Simba", age: "2", sex: "M", image: "insert image here")
-      starla = shelter_2.pets.create(name: "Starla", age: "18", sex: "F", image: "insert image here")
-      jasper = shelter_2.pets.create(name: "Jasper", age: "11", sex: "M", image: "insert image here")
+      bishi = shelter_1.pets.create(name: "Bishi", age: "10", sex: "M", image: "https://ih.constantcontact.com/fs159/1104169690227/img/112.jpg?a=1118551010932")
+      simba = shelter_1.pets.create(name: "Simba", age: "2", sex: "M", image: "https://images.pexels.com/photos/736532/pexels-photo-736532.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260")
+      starla = shelter_2.pets.create(name: "Starla", age: "18", sex: "F", image: "https://images.pexels.com/photos/1605481/pexels-photo-1605481.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260")
+      jasper = shelter_2.pets.create(name: "Jasper", age: "11", sex: "M", image: "https://images.pexels.com/photos/1543793/pexels-photo-1543793.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260")
 
       visit "/pets"
 
-      expect(page).to have_content(bishi.name)
-      expect(page).to have_content(simba.age)
-      expect(page).to_not have_content(starla.sex)
-      expect(page).to_not have_content(shelter.name)
-      expect(page).to_not have_content(jasper.image)
+      expect(page).to have_content("Pet: #{bishi.name}")
+      expect(page).to have_content("Approximate Age: #{simba.age}")
+      expect(page).to have_content("Sex: #{starla.sex}")
+      expect(page).to have_css("img[src*='#{jasper.image}']")
+      expect(page).to have_content("Located at: #{shelter_1.name}")
     end
   end
 end
+
 
 # User Story 7, Pet Index
 #
