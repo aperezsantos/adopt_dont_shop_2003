187ce037f641a147063415f23b9fd842b9080f98
Ana Perez Santos <a.jperez@yahoo.com>
2020-05-10 04:04:36 -0600

    Update shelter pets index spec file and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ Pet.destroy_all
 shelter_1 = Shelter.create(name: "Cat Care Society", address: "5787 W 6th Ave", city: "Lakewood", state: "CO", zip: "80214")
 shelter_2 = Shelter.create(name: "Foothils Animal Shelter", address: "580 McIntyre St", city: "Golden", state: "CO", zip: "80401")
 
-bishi = shelter_1.pets.create(image: "https://ih.constantcontact.com/fs159/1104169690227/img/112.jpg?a=1118551010932", name: "Bishi", age: "10", sex: "M")
-simba = shelter_1.pets.create(image: "https://images.pexels.com/photos/736532/pexels-photo-736532.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260", name: "Simba", age: "2", sex: "M")
-starla = shelter_2.pets.create(image: "https://images.pexels.com/photos/1605481/pexels-photo-1605481.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260", name: "Starla", age: "18", sex: "F")
-jasper = shelter_2.pets.create(image: "https://images.pexels.com/photos/1543793/pexels-photo-1543793.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260", name: "Jasper", age: "11", sex: "M")
+bishi = shelter_1.pets.create(image: "https://ih.constantcontact.com/fs159/1104169690227/img/112.jpg?a=1118551010932", name: "Bishi", age: "10", sex: "M", adoption_status: "Adoptable")
+simba = shelter_1.pets.create(image: "https://images.pexels.com/photos/736532/pexels-photo-736532.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260", name: "Simba", age: "2", sex: "M", adoption_status: "Pending")
+starla = shelter_2.pets.create(image: "https://images.pexels.com/photos/1605481/pexels-photo-1605481.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260", name: "Starla", age: "18", sex: "F", adoption_status: "Pending")
+jasper = shelter_2.pets.create(image: "https://images.pexels.com/photos/1543793/pexels-photo-1543793.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260", name: "Jasper", age: "11", sex: "M", adoption_status: "Adoptable")
--- spec/features/pets/shelter_pets_new_spec.rb
+++ spec/features/pets/shelter_pets_new_spec.rb
@@ -3,31 +3,30 @@ require 'rails_helper'
 RSpec.describe "Shelter Pets Index" do
   describe "when I visit a shelters pets index page" do
     it "I can click on a link that lets me add a new adoptable pet for that shelter" do
-      shelter_1 = Shelter.create(name: "Cat Care Society", address: "5787 W 6th Ave", city: "Lakewood", state: "CO", zip: "80214")
-      shelter_2 = Shelter.create(name: "Foothils Animal Shelter", address: "580 McIntyre St", city: "Golden", state: "CO", zip: "80401")
+      shelter = Shelter.create(name: "Foothils Animal Shelter", address: "580 McIntyre St", city: "Golden", state: "CO", zip: "80401")
 
-      bishi = shelter_1.pets.create(image: "https://ih.constantcontact.com/fs159/1104169690227/img/112.jpg?a=1118551010932", name: "Bishi", age: "10", sex: "M")
-      simba = shelter_1.pets.create(image: "https://images.pexels.com/photos/736532/pexels-photo-736532.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260", name: "Simba", age: "2", sex: "M")
-      starla = shelter_2.pets.create(image: "https://images.pexels.com/photos/1605481/pexels-photo-1605481.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260", name: "Starla", age: "18", sex: "F")
-      jasper = shelter_2.pets.create(image: "https://images.pexels.com/photos/1543793/pexels-photo-1543793.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260", name: "Jasper", age: "11", sex: "M")
+      starla = shelter.pets.create(image: "https://images.pexels.com/photos/1605481/pexels-photo-1605481.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260", name: "Starla", age: "18", sex: "F", adoption_status: "Pending")
+      jasper = shelter.pets.create(image: "https://images.pexels.com/photos/1543793/pexels-photo-1543793.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260", name: "Jasper", age: "11", sex: "M", adoption_status: "Adoptable")
 
-      visit "/shelters/#{shelter_2.id}/pets/new"
+      visit "/shelters/#{shelter.id}/pets/new"
 
       fill_in "image", with: "http://1.bp.blogspot.com/-FN50PT1n9KA/Ui5RgNJMTBI/AAAAAAAAeyk/Ns7K6hjkKWQ/s1600/IMG_9531_1_1-731935.JPG"
       fill_in "name", with: "New Pet Name"
       fill_in "age", with: "New Pet Age"
       fill_in "sex", with: "New Pet Sex"
       fill_in "description", with: "New Pet Description"
+      fill_in "adoption_status", with: "New Pet Adoption Status"
 
       click_on "Create Pet"
 
       new_pet = Pet.last
 
-      expect(current_path).to eq("/pets/#{new_pet.id}")
+      expect(current_path).to eq("/shelters/#{shelter.id}/pets")
       expect(page).to have_content(new_pet.name)
       expect(page).to have_content(new_pet.age)
       expect(page).to have_content(new_pet.sex)
-      expect(page).to have_content(new_pet.dscription)
+      expect(page).to have_content(new_pet.description)
+      expect(page).to have_content(new_pet.adoption_status)
     end
   end
 end
